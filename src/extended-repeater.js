f098f1df5537aee477d81ded1208907a7c986274
ITboo <annaacdc@tut.by>
2023-04-16 13:18:39 +0300

    feat: extended-repeater solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,52 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+
+  let repeat = 1;
+  let addRepeat = 1;
+  let separator = '+';
+  let addSeparator = '|';
+  let addition = '';
+
+  for (const property in options) {
+
+
+    if (property === 'repeatTimes') {
+      repeat = options[property];
+
+    }
+    else if (property === 'separator') {
+      separator = options[property];
+
+    }
+    else if (property === 'addition') {
+      addition = options[property];
+
+    }
+    else if (property === 'additionRepeatTimes') {
+      addRepeat = options[property];
+
+    }
+    else if (property === 'additionSeparator') {
+      addSeparator = options[property];
+
+    }
+
+  };
+
+  if ((addRepeat === 1)) { addSeparator = '' };
+
+  let result = '';
+  let addCall = (addition + addSeparator).repeat(addRepeat)
+  addCall = addCall.slice(0, addCall.length - addSeparator.length)
+  for (let i = 0; i < repeat; i++) {
+    result += str + addCall + separator
+  }
+
+  result = result.slice(0, result.length - separator.length)
+
+  return result
 }
 
 module.exports = {
