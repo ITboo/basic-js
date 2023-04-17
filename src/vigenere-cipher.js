e1dda26cc504c28fbe49f5ae5e6ca8caa4c07dff
ITboo <annaacdc@tut.by>
2023-04-17 16:49:28 +0300

    feat: vigenere-cipher solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,14 +19,53 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
+const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect) {
+    if (isDirect == false) this.reverse = true;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  decrypt(m, k) {
+    if (!m || !k) {
+      throw new Error("Incorrect arguments!");
+    }
+    m = m.toUpperCase();
+    k = k.toUpperCase();
+    let str = "";
+    let j = 0;
+    for (let i = 0; i < m.length; i++) {
+      if (alphabet.includes(m[i])) {
+        let mi = alphabet.indexOf(m[i]);
+        let ki_s = k[j >= k.length ? j % k.length : j];
+        let ki = alphabet.indexOf(ki_s);
+        let c = alphabet[(26 + mi - ki) % 26];
+        str += c;
+        j++;
+      } else str += m[i];
+    }
+    return this.reverse ? str.split("").reverse().join("") : str;
+  }
+
+  encrypt(m, k) {
+    if (!m || !k) {
+      throw new Error("Incorrect arguments!");
+    }
+    m = m.toUpperCase();
+    k = k.toUpperCase();
+    let str = "";
+    let j = 0;
+    for (let i = 0; i < m.length; i++) {
+      if (alphabet.includes(m[i])) {
+        let mi = alphabet.indexOf(m[i]);
+        let ki_s = k[j >= k.length ? j % k.length : j];
+        let ki = alphabet.indexOf(ki_s);
+        let c = alphabet[(mi + ki) % 26];
+        str += c;
+        j++;
+      } else str += m[i];
+    }
+    return this.reverse ? str.split("").reverse().join("") : str;
   }
 }
 
